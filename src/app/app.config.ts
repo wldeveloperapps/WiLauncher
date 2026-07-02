333357bb70001f4d67d11a28c0ec36654289e67e
Marta <marta.naveira@wiloc.com>
2026-07-02 08:33:32 +0200

    Update project dependencies II. no primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA19nFVsYUGDutcz17LeJsURQVoM48AOj0',
+    authDomain: 'wilauncher.firebaseapp.com',
+    projectId: 'wilauncher',
+    storageBucket: 'wilauncher.firebasestorage.app',
+    messagingSenderId: '775063189008',
+    appId: '1:775063189008:web:e4e4515dc9bd569209fc95',
+    measurementId: 'G-198LTZWTTC',
+  },
+};
